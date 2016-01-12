ca36d53397b10ca2dcb8611777dda89b3095fed5
PixelZerg <abhinavbhandari4@gmail.com>
2016-01-12 19:10:14 +0000

    Fade when changing tabs

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -58,10 +58,15 @@
      */
     var goToTab = function(index) {
       if (index !== activeIndex && index >= 0 && index <= tabNavigationLinks.length) {
-        tabNavigationLinks[activeIndex].classList.remove('is-active');
+        // $(tabNavigationLinks[activeIndex]).fadeOut('slow',function(){
+          tabNavigationLinks[activeIndex].classList.remove('is-active');
+      // });
         tabNavigationLinks[index].classList.add('is-active');
-        tabContentContainers[activeIndex].classList.remove('is-active');
-        tabContentContainers[index].classList.add('is-active');
+        $(tabContentContainers[activeIndex]).fadeOut('slow',function(){
+          tabContentContainers[activeIndex].classList.remove('is-active');
+          tabContentContainers[activeIndex].removeAttribute("style"); 
+          tabContentContainers[index].classList.add('is-active');
+      });
         activeIndex = index;
       }
     };
